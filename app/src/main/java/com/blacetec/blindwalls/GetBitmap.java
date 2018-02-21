e851479721cec5fb5b3c50904ede929dd45bfbdc
wjagottm <34193148+wjagottm@users.noreply.github.com>
2018-02-21 17:09:29 +0100

    Rename getByteArray to GetBitmap

---
+++ app/src/main/java/com/blacetec/blindwalls/GetBitmap.java
@@ -0,0 +1,33 @@
+package com.blacetec.blindwalls;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by arant on 2/21/2018.
+ */
+
+public class GetBitmap extends AsyncTask<String, Void, Bitmap> {
+
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        try {
+            URL url = new URL(strings[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            return BitmapFactory.decodeStream(input);
+        } catch (IOException e) {
+            Log.e("GetBitmap", "Convertion failed: " + e.getLocalizedMessage());
+            return null;
+        }
+    }
+}
